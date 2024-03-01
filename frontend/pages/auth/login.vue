a1bcf07323a37d481f9d1f8e3146d67a767e9b45
salman <therodfighter@gmail.com>
2024-03-01 22:35:23 +0530

    jkj

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -6,6 +6,7 @@ const auth = authStore();
 const isPwd = ref(true);
 const loading = ref(false)
 const config = useRuntimeConfig()
+const $q = useQuasar()
 
 const form = ref({
   email: '',
@@ -56,23 +57,24 @@ const login = async () => {
   <div class="row q--col-gutter-md q-pa-md q-pa-md-lg q-pa-md q-pa-lg-lg window-height" style="max-height: 100vh;">
     <div class="col-12 col-md-7 column full-height">
       <div class="col-1">
-        <h1 class="text-h4 text-weight-bold text-primary">Start up</h1>
+        <h1 class="text-h4 text-weight-bold text-primary">Sign in</h1>
       </div>
       <div class="col-11 row justify-center items-center">
-        <q-card class="my-card q-pa-md ">
-          <q-card-section>
+        
+        <q-card class="my-card q-pa-0 q-pa-sm-md" :class="$q.screen.lt.sm ? 'no-shadow full-width' : ''" :style="{ translate: '0px -50px', width: $q.screen.gt.xs ? '400px' : 'auto' }">
+          <q-card-section :class="$q.screen.lt.sm ? 'q-pa-none' : ''">
             <div class="row justify-center">
               <BrandLogo />
             </div>
             <div class="text-h5 text-weight-bold text-center">
               Sign in to your account
             </div>
-            <div class="text-body2 text-grey-8 text-center">
-              Please enter your crendtials to login
-            </div>
           </q-card-section>
-
-          <q-card-section class="q-pt-none">
+          <q-card-section class="row justify-center q-py-lg" :class="$q.screen.lt.sm ? 'q-pa-none' : ''">
+            <q-btn outline style="text-transform: none;" color="grey-8" class="full-width"><q-icon left name="img:/images/google-icon.webp"></q-icon> <div>  Sign in with Google</div></q-btn>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="q-pt-none q-pt-md" :class="$q.screen.lt.sm ? 'q-pa-none' : ''">
             <form class="q-gutter-y-lg" @submit.prevent="login">
               <div>
                 <label>Email</label>
@@ -86,8 +88,8 @@ const login = async () => {
                       @click="isPwd = !isPwd" />
                   </template>
                 </q-input>
-                <div class="row items-center justify-end p-2">
-                  <NuxtLink :to="routes.auth.admin_forgot_password" style="text-decoration: none; color: black">Forgot
+                <div class="row items-center justify-end q-pt-sm">
+                  <NuxtLink :to="routes.auth.admin_forgot_password" >Forgot
                     Password</NuxtLink>
                 </div>
               </div>
@@ -95,8 +97,11 @@ const login = async () => {
                 <q-circular-progress indeterminate size="20px" class="q-px-10" :thickness="1" color="grey-8"
                   track-color="orange-2" style="min-width: 8rem" />
               </q-btn>
-              <q-btn v-else type="submit" color="primary" style="width: 100%">Submit</q-btn>
+              <q-btn v-else type="submit" color="primary" style="width: 100%">Sign in</q-btn>
             </form>
+
+              <p class="text-right q-py-sm">Dont have an account? <NuxtLink to="ds">Create</NuxtLink></p>
+            
           </q-card-section>
         </q-card>
       </div>
